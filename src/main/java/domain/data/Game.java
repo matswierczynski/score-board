3384646e9eb9dac7b93fae31f2e547e4956b9bf1
Mateusz Świerczyński <mwy@spyro-soft.com>
2022-04-14 20:44:58 +0200

    MaSw: Implementation of startGame and getSummary methods. Added missing Equals and HashCode methods to Game to properly store those instances within collections.

--- src/main/java/domain/data/Game.java
+++ src/main/java/domain/data/Game.java
@@ -1,18 +1,26 @@
 package domain.data;
 
+import java.util.Objects;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor(staticName = "of")
 @Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Game {
 
+  @EqualsAndHashCode.Include
   private final Team homeTeam;
 
+  @EqualsAndHashCode.Include
   private final Team awayTeam;
 
   private int homeTeamScore;
 
   private int awayTeamScore;
 
+  boolean isValidGame() {
+    return Objects.nonNull(homeTeam) && Objects.nonNull(awayTeam);
+  }
 }
--- src/main/java/domain/data/ScoreBoard.java
+++ src/main/java/domain/data/ScoreBoard.java
@@ -1,5 +1,37 @@
 package domain.data;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ScoreBoard {
 
+  private final Set<Game> ongoingGames;
+
+  public ScoreBoard() {
+    this.ongoingGames = new HashSet<>();
+  }
+
+  public Game startGame(final Team homeTeam, final Team awayTeam) {
+    final var gameToStart = Game.of(homeTeam, awayTeam);
+
+    if (!gameToStart.isValidGame()) {
+      throw new IllegalArgumentException("Game cannot be started for a null Team");
+    }
+
+    if (isOngoingGame(gameToStart)) {
+      throw new IllegalStateException("Cannot start unfinished game");
+    }
+
+    ongoingGames.add(gameToStart);
+    return gameToStart;
+  }
+
+  public Set<Game> getSummary() {
+    return Collections.unmodifiableSet(ongoingGames);
+  }
+
+  private boolean isOngoingGame(final Game game) {
+    return ongoingGames.contains(game);
+  }
 }
